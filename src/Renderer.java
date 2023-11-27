ad02246738a43afae34187e8f8add383293cea52
BT_1N3_32 <@>
2023-11-27 10:22:35 -0500

    fixed the window

--- src/Renderer.java
+++ src/Renderer.java
@@ -12,25 +12,29 @@ import java.awt.image.VolatileImage;
 public class Renderer {
     private static JFrame frame;
     private static JLabel canvas;
-    private static int canvasWidth = 0;
-    private static int canvasHeight = 0;
+    private static Dimension canvasSize;
 
     private static final int GAME_WIDTH = 400;
     private static final int GAME_HEIGHT = 300;
 
-    private static final int gameWidth = 0;
-    private static final int gameHeight = 0;
+    private static int gameWidth = 0;
+    private static int gameHeight = 0;
     public static void init (){
+        Toolkit tools = Toolkit.getDefaultToolkit();
         frame = new JFrame();
         canvas = new JLabel();
+        canvasSize = tools.getScreenSize();
+        gameWidth = (int)canvasSize.getWidth();
+        gameHeight = (int)canvasSize.getHeight();
 
         frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-        canvas.setPreferredSize(new Dimension(canvasWidth, canvasHeight));
+        frame.setLocation(0,0);
+        canvas.setPreferredSize(new Dimension(canvasSize));
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.add(canvas);
         frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
+        //frame.setLocationRelativeTo(null);
         frame.pack();
         frame.setVisible(true);
 
@@ -49,13 +53,13 @@ public class Renderer {
 
                     Graphics graph = Img.getGraphics();
 
-                    graph.setColor(Color.BLUE);
+                    graph.setColor(new Color(170, 224, 242));
                     graph.fillRect(0,0, gameWidth, gameHeight);
                     //other stuff...
                     graph.dispose();
 
                     graph = canvas.getGraphics();
-                    graph.drawImage(Img, 0, 0, canvasWidth, canvasHeight, null);
+                    graph.drawImage(Img, 0, 0, new Color(170, 224, 242), null);
 
                     graph.dispose();
                 }
