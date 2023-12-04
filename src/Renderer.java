1d4b8d390df58ed925c8d38d4eea7e4f6543a3d8
ashifecchi <qiaolinm@nycstudents.net>
2023-12-03 21:15:05 -0500

    deleted stuff i dont need

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,18 +1,13 @@
 import java.awt.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.Font;
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.VolatileImage;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 
 public class Renderer implements ActionListener {
     private static JFrame frame;
